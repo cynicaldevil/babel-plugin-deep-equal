e697f8ac338b05c5110b58bae412cd94534fb465
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-06-26 03:40:07 +0530

    Dependencies: Added babel-core and webpack and related setup

---
+++ webpack.config.test.js
@@ -0,0 +1,43 @@
+var path =  require('path');
+var fs = require('fs');
+
+var nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(function(x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
+
+var config = {
+  entry: {
+    src: ["./test/test.js"]
+  },
+  target: 'node',
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      loaders: ['babel-loader'],
+      exclude: /node_modules/
+    }]
+  },
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'build.js',
+    libraryTarget: 'commonjs2'
+  },
+  resolve: {
+    extensions: ['', '.js'],
+    packageMains: ['webpack', 'browser', 'web', 'browserify', ['jam', 'main'], 'main']
+  },
+  plugins: [
+
+  ],
+  node: {
+    fs: "empty"
+  },
+  externals: [ nodeModules ]
+};
+
+module.exports = config;
