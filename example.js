a56cf2f98550c4c259d5bda8ef7b13f8697cdb6d
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-07-09 16:46:55 +0530

    Modified example.js to ensure prev change works

--- example.js
+++ example.js
@@ -1,2 +1,4 @@
-let var1 = 'dhhss';
-let number = 3;
+let abc = { num: 1, str: 'string!', bool: false };
+let def = { num: 123, str: 'str!', bool: true };
+console.log(abc === def);
+console.log( ghi + jkl );
