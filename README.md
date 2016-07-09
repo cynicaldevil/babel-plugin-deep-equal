0bfcde3df4a58bcbb9f9edb3fc4ed73defcde4c2
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-07-09 17:21:43 +0530

    Misc: Updated README and .gitignore

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 #deep-equal
 Babel plugin which allows use of triple equals operator ('===') to compare two objects
+This plugin can be used to compare any two objects for equality, irrespective of
+their types.
---
+++ src/test_only.js
@@ -0,0 +1,12 @@
+var arr1 = [1, 2, 3];
+var arr2 = [1, 2, 3];
+evalt(arr1);
+// console.log( Array.isArray(arr1));
+
+function evalt(arr)
+{
+  return true;
+  // console.log( Array.isArray(arr));
+}
+
+console.log(evalt.toString());
