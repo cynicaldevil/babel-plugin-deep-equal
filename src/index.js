1d8ae1838ab901cd90c85180c20cdc4b7998ac5f
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-07-09 16:46:09 +0530

    Added a visitor to binary expressions which replaces the expression with a source string

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 const deep_equal = (babel) => {
   const t = babel.types;
+
+  const deep_equal_sourceString = `
+    (function test() {
+      return true;
+    })()
+  `;
+
   return {
     visitor: {
       BinaryExpression(path) {
-        let name = path.node.name;
-        // reverse the name: JavaScript -> tpircSavaJ
-        path.node.name = name.split('').reverse().join('');
+
+        // return if this expression is not satisfied
+        // deep-equal does not perform operation on literals
+        const returnExp = path.node.operator=== '===' &&
+        t.isIdentifier(path.node.right) &&
+        t.isIdentifier(path.node.left);
+        if(!returnExp)
+          return;
+        path.replaceWithSourceString(deep_equal_sourceString);
       }
     }
   }
