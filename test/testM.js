9ea4719f01a4ebce457cfab88d78f4ba06ab1c38
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-07-22 17:17:28 +0530

    Tests:Added mocha and chai for testing

---
+++ test/testM.js
@@ -0,0 +1,8 @@
+const assert = require('chai').assert;
+describe('module', function() {
+  describe('nestedModule', function() {
+    it('should check if two strings are equal', function() {
+      assert.equal('asd', 'asd');
+    });
+  });
+});
