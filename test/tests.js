ed84ca9fc1051cba2beb1bb6bc72e33b5cbc43dd
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-08-02 00:21:15 +0530

    Added test for strings

--- test/tests.js
+++ test/tests.js
@@ -2,5 +2,10 @@ const assert = require('chai').assert;
 import deep_equal from '../src/deep_equal_fn';
 
 describe('deep Equal', function() {
+  it('should check if two strings are equal or not', function() {
 
+    assert.equal(deep_equal('asdfghjkl', 'asdfghjkl'), true);
+    assert.equal(deep_equal('asdfghjkl', 'qwertyuiop'), false);
+
+  });
 });
